541ada77023efc8877d67b64fb18971f7676541d
Sam Baumgarten <sam@anchorlabs.io>
2019-04-14 18:46:35 -0400

    Add training flag to Dropout

--- keras/layers/core.py
+++ keras/layers/core.py
@@ -100,12 +100,13 @@ class Dropout(Layer):
            http://www.jmlr.org/papers/volume15/srivastava14a/srivastava14a.pdf)
     """
     @interfaces.legacy_dropout_support
-    def __init__(self, rate, noise_shape=None, seed=None, **kwargs):
+    def __init__(self, rate, noise_shape=None, seed=None, training=False, **kwargs):
         super(Dropout, self).__init__(**kwargs)
         self.rate = min(1., max(0., rate))
         self.noise_shape = noise_shape
         self.seed = seed
         self.supports_masking = True
+        self.train_only = not training
 
     def _get_noise_shape(self, inputs):
         if self.noise_shape is None:
@@ -119,12 +120,12 @@ class Dropout(Layer):
     def call(self, inputs, training=None):
         if 0. < self.rate < 1.:
             noise_shape = self._get_noise_shape(inputs)
-
+            in_training = training if self.train_only == True else True
             def dropped_inputs():
                 return K.dropout(inputs, self.rate, noise_shape,
                                  seed=self.seed)
             return K.in_train_phase(dropped_inputs, inputs,
-                                    training=training)
+                                    training=in_training)
         return inputs
 
     def get_config(self):
